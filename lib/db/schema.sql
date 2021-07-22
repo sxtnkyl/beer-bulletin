64756385c2afebaaffce05c5bb65dd826899efa5
kyle sexton <ksexton.exe@gmail.com>
2021-07-22 11:50:29 -0400

    started dev setup for local mysql, added packages, signup form

---
+++ lib/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS beer_bulletin_Users;
+DROP DATABASE IF EXISTS beer_bulletin_Chats;
+DROP DATABASE IF EXISTS beer_bulletin_Trades;
+DROP DATABASE IF EXISTS beer_bulletin_Beers;
+
+CREATE DATABASE beer_bulletin_Users;
+CREATE DATABASE beer_bulletin_Chats;
+CREATE DATABASE beer_bulletin_Trades;
+CREATE DATABASE beer_bulletin_Beers;
+
+-- populate with data
