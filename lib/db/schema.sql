97009bdad733f11bfba9055279f0657de49a0e1e
bob.w.perez <bob.w.perez@gmail.com>
2021-07-24 08:16:05 -0400

    added JSON seed data for users and trades

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -2,6 +2,9 @@ DROP DATABASE IF EXISTS beer_bulletin;
 
 CREATE DATABASE beer_bulletin;
 
+
+
+-- WE WONT NEED THESE, SEQUELIZE WILL MAKE TABLES WITH MODEL INFO
 CREATE TABLE beer_bulletin_Users;
 CREATE TABLE beer_bulletin_Chats;
 CREATE TABLE beer_bulletin_Trades;
