d2d3e0e8eac2fa1825e7faee8491f7ecf9d1f09b
Austin Huffman <austinhuffman0@gmail.com>
2021-08-09 11:48:00 -0400

    Post Creation Form Skeleton

--- components/forms/CreatePostForm.jsx
+++ components/forms/CreatePostForm.jsx
@@ -1,23 +1,64 @@
 import React from 'react';
 import * as C from '@material-ui/core';
 
-
-// Yeah i realize this is messy. Gonna clean it up. 
-// Right Now we really are only submitting 2 things
+const useStyles = C.makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        
+      },
+    },
+}));
 
 export default function createPostForm() {
+
+    const classes = useStyles();
+    const [value, setValue] = React.useState('Controlled');
+  
+    const handleChange = (event) => {
+      setValue(event.target.value);
+    };
+
     return(
-        <div style={{display:'flex',justifyContent:'center', marginTop:'5em'}}>
-            <C.Card style={{display: 'inline-block'}} >
+        <div style={{display:'flex',justifyContent:'center', flexBasis:"100px" ,marginTop:'5em'}}>
+            <C.Card >
                 <C.CardContent align="center" >
                     <h1>Create Post</h1>
+                    <hr/>
                     <form>
-                   
-                    <C.TextField id="post-title" label="Title" variant="outlined" />
-                    <br />
-                    <C.TextField id="post-description" label="Description" variant="outlined" />
-                    <br />
-                    {/* IMAGE SUBMIT */}
+                    <br/>
+                    <C.TextField 
+                        id="post-title" 
+                        label="Title" 
+                        variant="outlined" />
+
+                    <br/><br/>
+                    
+
+                    <C.TextField
+                        id="outlined-multiline-static"
+                        label="Describe Your Offer"
+                        multiline
+                        rows={4}
+                        defaultValue=""
+                        variant="outlined"
+                     />
+                    <br /><br />
+                    <C.Button variant="contained" component="label">
+                       
+                    Upload File
+                    <input
+                        type="file"
+                        hidden
+                    />
+                    </C.Button>
+
+                    <br /><br /><hr /><br />
+
+                    <C.Button variant="contained" color="primary" href="#">
+                        Submit
+                    </C.Button>
+
                     </form>
                 </C.CardContent>
             </C.Card>
