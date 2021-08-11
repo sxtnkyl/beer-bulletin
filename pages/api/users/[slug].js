93b5c13fc7f3e589048f369ab369a12abe106f82
bob.w.perez <bob.w.perez@gmail.com>
2021-08-11 04:27:42 -0400

    updated user slug route to show just user info (no joins)

--- pages/api/users/[slug].js
+++ pages/api/users/[slug].js
@@ -1,30 +1,20 @@
-import nextConnect from 'next-connect';
-const models = require('../../../db/models/index');
+import nextConnect from "next-connect";
+const models = require("../../../db/models/index");
 
 const handler = nextConnect()
   .get(async (req, res) => {
-    const {
-      query: { id, name },
-    } = req;
+    // const {
+    //   query: { id, name },
+    // } = req;
     const { slug } = req.query;
     const userId = slug;
     const user = await models.users.findOne({
       where: {
         id: userId,
       },
-      include: [
-        {
-          model: models.posts,
-          as: 'posts',
-        },
-        {
-          model: models.jobs,
-          as: 'jobs',
-        },
-      ],
     });
     res.statusCode = 200;
-    return res.json({ status: 'success', data: user });
+    return res.json({ status: "success", data: user });
   })
   .post(async (req, res) => {})
   .put(async (req, res) => {})
