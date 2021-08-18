077104a390f150f2cabfdb2aa1fd883fc2b3dd52
Austin Huffman <austinhuffman0@gmail.com>
2021-08-18 13:38:48 -0400

    Chat Framework

--- pages/CurrentChats/index.jsx
+++ pages/CurrentChats/index.jsx
@@ -15,7 +15,7 @@ const useStyles = makeStyles({
   },
   chatSection: {
     width: '100%',
-    height: '80vh'
+    height: '80vh',
   },
   headBG: {
       backgroundColor: '#e0e0e0'
@@ -25,7 +25,10 @@ const useStyles = makeStyles({
   },
   messageArea: {
     height: '70vh',
-    overflowY: 'auto'
+    overflowY: 'auto',
+  },
+  typeArea: {
+    padding: '0px 10px'
   }
 });
 
@@ -35,6 +38,7 @@ const CurrentChats = () => {
   return (
     <div>
       <div>
+      <C.Grid container>
         <C.Grid container>
             <C.Grid item xs={12} >
                 <C.Typography variant="h5" className="header-message">Chat</C.Typography>
@@ -45,51 +49,20 @@ const CurrentChats = () => {
             <C.Grid item xs={12}>
                 <C.List className={style.messageArea}>
 
-  {/* ALTERNATE LEFT AND RIGHT MESSAGES */}
-  {/* I WILL MOVE THESE TO INDIVIDUAL MESSAGE PANESL THAT ALIGN LEFT OR RIGHT DEPENDING ON THE PERSON */}
-  
-                    <C.ListItem key="1">
-                        <C.Grid container>
-                            <C.Grid item xs={12}>
-                                <C.ListItemText align="right" primary="Hi I'm a message!"></C.ListItemText>
-                            </C.Grid>
-                            <C.Grid item xs={12}>
-                                <C.ListItemText align="right" secondary="09:30"></C.ListItemText>
-                            </C.Grid>
-                        </C.Grid>
-                    </C.ListItem>
-                    
-                    <C.ListItem key="2">
-                        <C.Grid container>
-                            <C.Grid item xs={12}>
-                                <C.ListItemText align="left" primary="Woah Message What's up my guy"></C.ListItemText>
-                            </C.Grid>
-                            <C.Grid item xs={12}>
-                                <C.ListItemText align="left" secondary="07:32"></C.ListItemText>
-                            </C.Grid>
-                        </C.Grid>
-                    </C.ListItem>
+  {/* ALTERNATE LEFT AND RIGHT MESSAGES */}  
 
-                    <C.ListItem key="3">
-                        <C.Grid container>
-                            <C.Grid item xs={12}>
-                                <C.ListItemText align="right" primary="NM. Just Chillin and being a message"></C.ListItemText>
-                            </C.Grid>
+                {/* insert messagepanel.js here */}
 
-                            <C.Grid item xs={12}>
-                                <C.ListItemText align="right" secondary="8:21"></C.ListItemText>
-                            </C.Grid>
-                        </C.Grid>
-                    </C.ListItem>
-                    
+            
+
                 </C.List>
                 <C.Divider />
-                <C.Grid container style={{}}>
-                   
-                        <C.TextField id="outlined-basic-email" label="Type Something" fullWidth />
+                <C.Grid className= {style.typeArea} >
+                        <C.TextField id="outlined-basic-email" label="Username" fullWidth/>
         
                 </C.Grid>
             </C.Grid>
+        </C.Grid>
         </C.Grid>
         </div>
     </div>
