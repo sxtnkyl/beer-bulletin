459fb3ba75d1f31faafbbf0b4528f2de71b546d1
bob.w.perez <bob.w.perez@gmail.com>
2021-08-24 19:51:54 -0400

    beer pong added to chat slug

---
+++ components/beerPong/BeerPongGame.jsx
@@ -0,0 +1,19 @@
+import React, { useLayoutEffect, useRef, useState } from "react";
+
+const BeerPongGame = () => {
+  return (
+    <div
+      className="beer-pong"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <iframe
+        title="bpGame"
+        src="/beerPong/beerPong.html"
+        width="80%"
+        height="600px"
+      ></iframe>
+    </div>
+  );
+};
+
+export default BeerPongGame;
