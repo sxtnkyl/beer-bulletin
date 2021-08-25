5bc0ec9c812537c74eb8e0a971771bd3deb46e26
bob.w.perez <bob.w.perez@gmail.com>
2021-08-25 11:41:46 -0400

    updated API routes to give new info

--- pages/api/users/with-offers/[slug].js
+++ pages/api/users/with-offers/[slug].js
@@ -33,7 +33,14 @@ const handler = nextConnect()
         {
           model: models.offers,
           as: "offers_made",
-          attributes: ["id", "participant_id", "resolved"],
+          attributes: [
+            "id",
+            "participant_id",
+            "resolved",
+            "offer_money",
+            "offer_beer",
+            "offer_other",
+          ],
           include: [
             {
               model: models.users,
