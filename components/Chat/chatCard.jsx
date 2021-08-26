f27723c974e05ad01bbd7e13c4e8724bddfca121
bob.w.perez <bob.w.perez@gmail.com>
2021-08-25 23:41:03 -0400

    fixed message retrieval issue due to ?=[id] being in asPath when navigating from main chat page

--- components/Chat/chatCard.jsx
+++ components/Chat/chatCard.jsx
@@ -6,50 +6,45 @@ import GlassCard from "../glassCard";
 import ScalableIcon from "../ScalableIcon";
 import theme from "../../styles/theme";
 
-
 const useStyles = C.makeStyles((theme) => ({
-    card: {
-      display: "flex",
-    },
-    stretch: {
-      display: "flex",
-      flex: "1 1 auto",
-      alignItems: "stretch",
-    },
-    content: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-around",
-    },
-    bot: {
-      flex: "1 1 auto",
-      width: "100%",
-    },
-  }));
+  card: {
+    display: "flex",
+  },
+  stretch: {
+    display: "flex",
+    flex: "1 1 auto",
+    alignItems: "stretch",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+  },
+  bot: {
+    flex: "1 1 auto",
+    width: "100%",
+  },
+}));
 
-  const ChatCard = (props) => {
-    const {id, offer_money, offer_beer, offer_other, host, trade } = props;
-    const classes = useStyles();
-    
+const ChatCard = (props) => {
+  const { id, offer_money, offer_beer, offer_other, host, trade } = props;
+  const classes = useStyles();
+
   const chatBlock = (
     <Link passHref href={`/CurrentChats/${id}`}>
-    <C.CardActionArea className={classes.stretch}>
-      <C.CardContent className={classes.content}>
-        <h2>Username: {host.username}</h2>
-        <C.Divider variant="middle" />
-        {trade.title} <br />
-        {trade.content}
-        <C.Divider variant="middle" />
-      </C.CardContent>
-    </C.CardActionArea>
-  </Link>
-  )
-   
-      //
-    return (
-      <GlassCard className={classes.card}>
-      {chatBlock}
-    </GlassCard>
-    );
-  }
+      <C.CardActionArea className={classes.stretch}>
+        <C.CardContent className={classes.content}>
+          <h2>Username: {host.username}</h2>
+          <C.Divider variant="middle" />
+          {trade.title} <br />
+          {trade.content}
+          <C.Divider variant="middle" />
+        </C.CardContent>
+      </C.CardActionArea>
+    </Link>
+  );
+
+  //
+  return <GlassCard className={classes.card}>{chatBlock}</GlassCard>;
+};
 export default ChatCard;
