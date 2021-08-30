926db9d6cf468570d7ac9196b9368abd57bb1f1c
kyle sexton <ksexton.exe@gmail.com>
2021-08-30 16:00:24 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,35 @@
 module.exports = {
-  reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
   api: {
     bodyParser: {
-      sizeLimit: "2mb",
+      sizeLimit: '2mb',
     },
+  },
+  env: {
+    api: process.env.PORT + 'api/',
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    //   mySecret: 'secret',
+    //   secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+    apiUrl: 'api',
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/public',
+  },
+  // webpack(config, options) {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
+    config.node = {
+      ...(config.node || {}),
+      net: 'empty',
+      tls: 'empty',
+      dns: 'empty',
+      fs: 'empty',
+    };
+    return config;
   },
 };
