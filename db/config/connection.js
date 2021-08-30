8a95b82a6125474d74967df507537b6ea922b56f
bob.w.perez <bob.w.perez@gmail.com>
2021-08-30 16:37:59 -0400

    uh.....

--- db/config/connection.js
+++ db/config/connection.js
@@ -5,19 +5,26 @@ require("dotenv").config();
 
 let sequelize;
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: process.env.DB_HOST,
-      dialect: "mysql",
-      port: process.env.DB_PORT,
-    }
-  );
-}
+// if (config.use_env_variable) {
+sequelize = new Sequelize(process.env[config.use_env_variable], {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+});
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//       host: process.env.DB_HOST,
+//       dialect: "mysql",
+//       port: process.env.DB_PORT,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
